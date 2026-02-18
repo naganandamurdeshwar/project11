75ff173051db46a0e82e60e05893e0992004e54e
naganandamurdeshwar <naganandamurdeshwar2005@gmail.com>
2026-02-18 20:53:52 +0530

    1

--- src/component/New1.tsx
+++ src/component/New1.tsx
@@ -1,9 +1,19 @@
 import './new1.css'
 import img1 from '../assets/img11.jpg'
+import img2 from '../assets/img12.jpg'
+import img3 from '../assets/img13.jpg'
+import img4 from '../assets/img4.jpeg'
 function New1(){
     return(
         <div className='main'>
-            <div className='m1'></div>
+            <div className='m1'>
+                <div className='mt1'>
+                    <p>Browse Celebrities</p>
+                </div>
+                <div className='mt2'>
+                    <p>Find the perfect celebrity for your special occasion</p>
+                </div>
+            </div>
             <div className='m2'></div>
             <div className='m4'>
                 <div className='bottom1'>
@@ -13,15 +23,21 @@ function New1(){
                     <div className='bt2'></div>
                 </div>
                 <div className='bottom1'>
-                    <div className='bt1'></div>
+                    <div className='bt1'>
+                        <img src={img2} alt="" className='k1' />
+                    </div>
                     <div className='bt2'></div>
                 </div>
                 <div className='bottom1'>
-                    <div className='bt1'></div>
+                    <div className='bt1'>
+                        <img src={img3} alt="" className='k1'/>
+                    </div>
                     <div className='bt2'></div>
                 </div>
                 <div className='bottom1'>
-                    <div className='bt1'></div>
+                    <div className='bt1'>
+                        <img src={img4} alt="" className='k1'/>
+                    </div>
                     <div className='bt2'></div>
                 </div>
              </div>
