25ebe4fd6de7876be92e30e859d8970d2194995f
naganandamurdeshwar <naganandamurdeshwar2005@gmail.com>
2026-02-18 21:51:29 +0530

    2

--- src/component/New1.tsx
+++ src/component/New1.tsx
@@ -8,37 +8,90 @@ function New1(){
         <div className='main'>
             <div className='m1'>
                 <div className='mt1'>
-                    <p>Browse Celebrities</p>
+                    <p className='text1'>Browse Celebrities</p>
                 </div>
                 <div className='mt2'>
-                    <p>Find the perfect celebrity for your special occasion</p>
+                    <p className='text2'>Find the perfect celebrity for your special occasion</p>
                 </div>
             </div>
             <div className='m2'></div>
+            <div className='m5'>
+                <p className='text3'>Showing 20 Celebrities</p>
+            </div>
             <div className='m4'>
                 <div className='bottom1'>
                     <div className='bt1'>
                         <img src={img1} alt="" className='k1' />
                     </div>
-                    <div className='bt2'></div>
+                    <div className='bt2'>
+                        <div className='j1'>
+                            <p className='text5'>Aisha khan</p>
+                        </div>
+                        <div className='j2'>
+                            <p className='text4'>influencers</p>
+                        </div>
+                        <div className='j3'>
+                         <div className='o1'>
+                            
+                         </div>
+                         <div className='o2'></div>
+                         <div className='o3'></div>
+                        </div>
+                    </div>
                 </div>
                 <div className='bottom1'>
                     <div className='bt1'>
                         <img src={img2} alt="" className='k1' />
                     </div>
-                    <div className='bt2'></div>
+                    <div className='bt2'>
+                        <div className='j1'>
+                            <p className='text5'>Tanvi Mehta</p>
+                        </div>
+                        <div className='j2'>
+                        <p className='text4'>YouTubers</p>
+                        </div>
+                        <div className='j3'>
+                         <div className='o1'></div>
+                         <div className='o2'></div>
+                         <div className='o3'></div>
+                        </div>
+                    </div>
                 </div>
                 <div className='bottom1'>
                     <div className='bt1'>
                         <img src={img3} alt="" className='k1'/>
                     </div>
-                    <div className='bt2'></div>
+                    <div className='bt2'>
+                        <div className='j1'>
+                             <p className='text5'>Meera Patel</p>
+                        </div>
+                        <div className='j2'>
+                            <p className='text4'>influencers</p>
+                        </div>
+                        <div className='j3'>
+                         <div className='o1'></div>
+                         <div className='o2'></div>
+                         <div className='o3'></div>
+                        </div>
+                    </div>
                 </div>
                 <div className='bottom1'>
                     <div className='bt1'>
                         <img src={img4} alt="" className='k1'/>
                     </div>
-                    <div className='bt2'></div>
+                    <div className='bt2'>
+                         <div className='j1'>
+                             <p className='text5'>Amit sharma</p>
+                         </div>
+                        <div className='j2'>
+                             <p className='text4'>Comedians</p>
+                        </div>
+                        <div className='j3'>
+                         <div className='o1'></div>
+                         <div className='o2'></div>
+                         <div className='o3'></div>
+                        </div>
+                    </div>
                 </div>
              </div>
         </div>
